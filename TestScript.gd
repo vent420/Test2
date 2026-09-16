285f14712f4733d21b784c157728008df2c06e5c
vent420 <vincentmott2006@gmail.com>
2026-09-16 15:04:22 +0200

    Revert "Changed TestScript"
    This reverts commit 1d237f0077979381ceb8dda8b05c19a26f7ebc99.

--- TestScript.gd
+++ TestScript.gd
@@ -5,8 +5,7 @@ extends Node
 func _ready() -> void:
 	pass # Replace with function body.
 
-func _Test(delta: float) -> void:
-	
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
